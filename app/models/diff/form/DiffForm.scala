ed7ff88a782846f844d688dfea98c3424b151a46
Matthew Hotchen <github@mhn.me>
2016-06-15 07:40:42 +0100

    Add form validation

--- app/models/diff/form/DiffForm.scala
+++ app/models/diff/form/DiffForm.scala
@@ -1,12 +1,22 @@
 package models.diff.form
 
+import models.diff.{Diff, DiffError}
 import play.api.data.Forms._
 import play.api.data._
+import play.api.data.validation._
 
 object DiffForm {
+  val diffConstraint: Constraint[String] = Constraint("constraints.diff") {
+    plainText =>
+      Diff(plainText) match {
+        case DiffError(e) => Invalid(ValidationError(e))
+        case Diff(_) => Valid
+      }
+  }
+
   val form = Form(
     mapping(
-      "diff" -> nonEmptyText
+      "diff" -> nonEmptyText(maxLength = 10240).verifying(diffConstraint)
     )(DiffFields.apply)(DiffFields.unapply)
   )
 }
